d93a07738692f17c5003fa42b1e734e30cf4dae0
Permlap <permlap@tatugacamp.com>
2025-11-20 17:10:19 +0700

    Increase JWT refresh token expiration time from 3 days to 7 days (#171)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -532,7 +532,7 @@ export class AuthService {
       };
       return await this.jwtService.signAsync(payload, {
         secret: this.config.get('JWT_REFRESH_SECRET'),
-        expiresIn: '3d',
+        expiresIn: '7d',
       });
     } catch (error) {
       this.logger.error(error);
@@ -570,7 +570,7 @@ export class AuthService {
       };
       return await this.jwtService.signAsync(payload, {
         secret: this.config.get('STUDENT_JWT_REFRESH_SECRET'),
-        expiresIn: '3d',
+        expiresIn: '7d',
       });
     } catch (error) {
       this.logger.error(error);
