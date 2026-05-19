0ade5ccf8c2447ebd2fae684d66db14c9dce4bda
Permlap Phola <permlap@tatugacamp.com>
2026-05-19 12:22:59 +0700

    refactor(auth): update UserGuard to use UserJwtPayload type and remove email verification check

--- src/auth/guard/user.guard.ts
+++ src/auth/guard/user.guard.ts
@@ -4,15 +4,13 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { createFastifyPassportGuard } from './fastify-passport.guard';
+import { UserJwtPayload } from '../../interfaces/jwt-payload';
 
 @Injectable()
 export class UserGuard extends createFastifyPassportGuard('user-jwt') {
-  handleRequest(err: unknown, user: any): any {
+  handleRequest(err: unknown, user: UserJwtPayload): any {
     if (err || !user) {
       throw new UnauthorizedException('Access denied');
-    }
-    if (user.isVerifyEmail === false) {
-      throw new ForbiddenException('Email not verified');
     }
     return user;
   }
